2abad737933cfbfef7e615cc4436aba52336a4fd
vitmistina <vit.mistina@gmail.com>
2025-12-21 08:48:42 +0100

    Implement input loading and validation

---
+++ terra_invicta_tech_optimizer/__init__.py
@@ -0,0 +1,14 @@
+"""Core package for Terra Invicta tech optimizer utilities."""
+
+from .input_loader import InputLoader, LoadReport, Node, NodeType
+from .validation import GraphValidator, ValidationIssue, ValidationResult
+
+__all__ = [
+    "InputLoader",
+    "LoadReport",
+    "Node",
+    "NodeType",
+    "GraphValidator",
+    "ValidationIssue",
+    "ValidationResult",
+]
